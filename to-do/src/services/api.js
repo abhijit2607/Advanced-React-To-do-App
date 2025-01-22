6ef35295ac1d9b44b20487e5f3215610a5455ab3
Abhijit <paulabhijit2607@gmail.com>
2025-01-22 22:17:54 +0530

    Update the API to a https for security reasons.

--- to-do/src/services/api.js
+++ to-do/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://api.weatherapi.com/v1',
+  baseURL: 'https://api.weatherapi.com/v1',
 });
 
 export const getWeather = (city) => api.get(`/forecast.json?key=8b781be9ead24848b9f161320252101&q=${city}&days=3`);
